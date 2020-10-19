96e9e2bbbdaed7f1e40f4312a1d7d44480f99689
Michael Habib <1233256+MichaelHabib@users.noreply.github.com>
2020-10-19 12:49:14 +1100

    M  .gitignore M  assets/main.js M  package.json M  source/index.js M  yarn.lock

--- source/index.js
+++ source/index.js
@@ -1,5 +1,5 @@
 import * as _ from 'lodash';
 //import './css/plain.css';
-import './js/magnific-popup.custom.js';  
-import './js/custom.js';  
-import './scss/main.scss';  
+import './js/magnific-popup.custom.js';
+import './js/custom.js';
+import './scss/main.scss';
